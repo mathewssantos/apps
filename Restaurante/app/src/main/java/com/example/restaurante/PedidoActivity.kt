b1db8a54d261f85dfb9d942ab04b59ec5a271c72
mathewssantos <166643506+mathewssantos@users.noreply.github.com>
2024-05-07 14:26:38 -0300

    PedidoActivity.kt

--- Restaurante/app/src/main/java/com/example/restaurante/PedidoActivity.kt
+++ Restaurante/app/src/main/java/com/example/restaurante/PedidoActivity.kt
@@ -2,10 +2,30 @@ package com.example.restaurante
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.restaurante.databinding.ActivityPedidoBinding
 
 class PedidoActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPedidoBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Infla o layout da atividade de pedido usando o vinculador gerado pelo View Binding
+        binding = ActivityPedidoBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pedido)
+        setContentView(binding.root)
+
+        // Obtém os valores de quantidade de pizza, salada e hambúrguer passados da atividade anterior
+        val i = intent
+        val quantidadePizza = i.getStringExtra("quantidadePizza").toString().toInt()
+        val quantidadeSalada = i.getStringExtra("quantidadeSalada").toString().toInt()
+        val quantidadeHamburguer = i.getStringExtra("quantidadeHamburguer").toString().toInt()
+
+        // Calcula o texto do resumo do pedido com base nas quantidades e preços
+        val texto = "Resumo do Pedido\n" +
+                "Pizza: $quantidadePizza Preço: ${quantidadePizza * 8}\n" +
+                "Salada: $quantidadeSalada Preço: ${quantidadeSalada * 10}\n" +
+                "Hamburguer: $quantidadeHamburguer Preço: ${quantidadeHamburguer * 12}\n"
+
+        // Define o texto do resumo do pedido no TextView correspondente
+        binding.textResumo.text = texto
     }
 }
