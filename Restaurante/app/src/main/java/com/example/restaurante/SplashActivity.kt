14e7240778875bf5c99aa8b7b5a1bcce733e302d
mathewssantos <166643506+mathewssantos@users.noreply.github.com>
2024-05-07 14:29:21 -0300

    SplashActivity.kt

--- Restaurante/app/src/main/java/com/example/restaurante/SplashActivity.kt
+++ Restaurante/app/src/main/java/com/example/restaurante/SplashActivity.kt
@@ -1,11 +1,25 @@
 package com.example.restaurante
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        // Cria um Handler para executar uma ação com atraso
+        Handler(Looper.getMainLooper()).postDelayed({
+            // Cria um Intent para iniciar a atividade de pedido
+            val i = intent
+            val j = Intent(this, PedidoActivity::class.java)
+            // Passa os extras do Intent atual para o Intent da atividade de pedido
+            j.putExtras(i)
+            // Inicia a atividade de pedido
+            startActivity(j)
+        }, 2000) // Define um atraso de 2000 milissegundos (2 segundos) antes de iniciar a próxima atividade
     }
 }
